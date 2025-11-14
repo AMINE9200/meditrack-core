47343c0e08a5e13f0b3e9df88165152db1c9877a
Amine <Amine.terraha@gmail.com>
2025-11-14 15:10:16 +0100

    Integrate auth routes into server configuration

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+// Routes
+import authRoutes from "./src/routes/authRoutes.js";
 import produitRoutes from "./src/routes/produitRoutes.js";
 import depotRoutes from "./src/routes/depotRoutes.js";
 import mouvementRoutes from "./src/routes/mouvementRoutes.js";
 import zoneRoutes from "./src/routes/zoneRoutes.js";
 
-// Connexion aux bases
+// Connexion Mongo + Postgre
 import "./src/config/db.js";
 
 dotenv.config();
@@ -16,10 +18,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ---- ROUTES ----
+app.use("/auth", authRoutes);        // 🔐 REGISTER + LOGIN
 app.use("/produits", produitRoutes);
 app.use("/depots", depotRoutes);
 app.use("/mouvements", mouvementRoutes);
 app.use("/zones", zoneRoutes);
 
+// ---- SERVER ----
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
