e257e2483f3ba5cbfffc7fd3470d445a253efcc8
Amine <Amine.terraha@gmail.com>
2025-11-14 15:04:38 +0100

    Add User model with create and find functions

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,18 @@
-import pkg from "pg";
-const { Pool } = pkg;
+import pool from "../config/db.js";
 
-import dotenv from "dotenv";
-dotenv.config();
-
-const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-});
-
-// Create user
 export const createUser = async (username, hashedPassword, role = "user") => {
-  const result = await pool.query(
-    "INSERT INTO users (username, password, role) VALUES ($1, $2, $3) RETURNING *",
-    [username, hashedPassword, role]
-  );
+  const query = `
+    INSERT INTO users (username, password, role)
+    VALUES ($1, $2, $3)
+    RETURNING id, username, role;
+  `;
+  const values = [username, hashedPassword, role];
+  const result = await pool.query(query, values);
   return result.rows[0];
 };
 
-// Get user by username
-export const getUserByUsername = async (username) => {
-  const result = await pool.query(
-    "SELECT * FROM users WHERE username = $1",
-    [username]
-  );
+export const findUserByUsername = async (username) => {
+  const query = `SELECT * FROM users WHERE username = $1`;
+  const result = await pool.query(query, [username]);
   return result.rows[0];
 };
